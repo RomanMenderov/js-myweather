3e5bc18ae33274c5d876b6e9f7a2ad43bac8bbb6
Roman Menderov <vladimirovich3112@mail.ru>
2020-12-16 08:21:12 +0300

    add coverage treshold

--- jest.config.js
+++ jest.config.js
@@ -42,7 +42,13 @@ module.exports = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+    },
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,

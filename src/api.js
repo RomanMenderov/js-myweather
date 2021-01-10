845d32c112a479b70e939c8d072bcb7a1dce4dc6
Roman Menderov <vladimirovich3112@mail.ru>
2021-01-10 13:26:23 +0300

    fix const names

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 /** working with API start */
-const apiKey = "fa5292c9164722fbd4dd9fb5132d9ea9";
+const USER_CITY_API_URL = "https://get.geojs.io/v1/ip/geo.json";
+const MAP_CITY_URL = "https://static-maps.yandex.ru/1.x/?l=map";
+const WEATHER_CITY_API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const WEATHER_CITY_API_KEY = "fa5292c9164722fbd4dd9fb5132d9ea9";
 
 export async function getUserCity() {
   try {
-    const apiUrl = "https://get.geojs.io/v1/ip/geo.json";
-    const response = await fetch(apiUrl);
+    const response = await fetch(USER_CITY_API_URL);
     if (response.ok) {
       const result = await response.json();
       return result.city;
@@ -16,12 +18,11 @@ export async function getUserCity() {
 }
 
 export function getMapCityUrl(coordinates) {
-  const apiUrlBasic = "https://static-maps.yandex.ru/1.x/?l=map";
   const size = "400,400";
   const zoom = 10;
   const longitude = coordinates.lon;
   const latitude = coordinates.lat;
-  const apiUrl = `${apiUrlBasic}&ll=${longitude},${latitude}
+  const apiUrl = `${MAP_CITY_URL}&ll=${longitude},${latitude}
 &size=${size}&z=${zoom}&pt=${longitude},${latitude}`;
 
   return apiUrl;
@@ -29,8 +30,8 @@ export function getMapCityUrl(coordinates) {
 
 export async function getWeatherCity(cityName) {
   try {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather
-?q=${cityName}&units=metric&appid=${apiKey}`;
+    const apiUrl = `${WEATHER_CITY_API_URL}?q=${cityName}&units=metric
+&appid=${WEATHER_CITY_API_KEY}`;
     const response = await fetch(apiUrl);
     if (response.ok) {
       const result = await response.json();

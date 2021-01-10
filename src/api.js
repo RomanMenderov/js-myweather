4a43015ad31fd05d08c11686212594306299d8b6
Roman Menderov <vladimirovich3112@mail.ru>
2021-01-10 12:46:09 +0300

    delete boring comments

--- src/api.js
+++ src/api.js
@@ -19,12 +19,10 @@ export function getMapCityUrl(coordinates) {
   const apiUrlBasic = "https://static-maps.yandex.ru/1.x/?l=map";
   const size = "400,400";
   const zoom = 10;
-  //  const key = "ff1506d9-6fce-4710-8c9d-d5a2c11ce32b";
   const longitude = coordinates.lon;
   const latitude = coordinates.lat;
   const apiUrl = `${apiUrlBasic}&ll=${longitude},${latitude}
 &size=${size}&z=${zoom}&pt=${longitude},${latitude}`;
-  //  apiUrlBasic + `&ll=${longitude},${latitude}` + `&size=${size}&z=${zoom}`; //  + &key=${key};
 
   return apiUrl;
 }

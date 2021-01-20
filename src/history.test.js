9e54d0c4e3f2d7e89d237883e75d8ca7b292789d
Roman Menderov <vladimirovich3112@mail.ru>
2021-01-20 21:51:57 +0300

    using DRY and beforeall afterall

--- src/history.test.js
+++ src/history.test.js
@@ -13,27 +13,34 @@ describe("test 1st task", () => {
   const setItemProto = window.localStorage.__proto__.setItem;
   const getItemProto = window.localStorage.__proto__.getItem;
 
+  beforeAll(() => {
+    jest.spyOn(window.localStorage.__proto__, "getItem");
+    window.localStorage.__proto__.getItem = jest.fn();
+
+    jest.spyOn(window.localStorage.__proto__, "setItem");
+    window.localStorage.__proto__.setItem = jest.fn();
+  });
+
   beforeEach(() => {
     userHistory = [];
   });
 
+  afterAll(() => {
+    window.localStorage.__proto__.getItem = getItemProto;
+    window.localStorage.__proto__.setItem = setItemProto;
+  });
+
   test("it should check user history", () => {
-    jest.spyOn(window.localStorage.__proto__, "getItem");
-    window.localStorage.__proto__.getItem = jest.fn();
     getUserHistory();
     expect(localStorage.getItem).toHaveBeenCalledWith(userHistoryLocalName);
-    window.localStorage.__proto__.getItem = getItemProto;
   });
 
   test("it should save new data in localstoradge", () => {
-    jest.spyOn(window.localStorage.__proto__, "setItem");
-    window.localStorage.__proto__.setItem = jest.fn();
     setUserHistory(["Minsk", "Moskow"]);
     expect(localStorage.setItem).toHaveBeenCalledWith(
       userHistoryLocalName,
       '["Minsk","Moskow"]'
     );
-    window.localStorage.__proto__.setItem = setItemProto;
   });
 
   test("it should check if city is unique ", () => {
@@ -49,17 +56,13 @@ describe("test 1st task", () => {
   });
 
   test("it should check if history data correct work", () => {
-    jest.spyOn(window.localStorage.__proto__, "setItem");
-    window.localStorage.__proto__.setItem = jest.fn();
     userHistory.push("Moscow");
     userHistory.push("Piter");
-
     saveCityToHistory("Vilnus", userHistory);
 
     expect(localStorage.setItem).toHaveBeenCalledWith(
       userHistoryLocalName,
       '["Vilnus","Moscow","Piter"]'
     );
-    window.localStorage.__proto__.setItem = setItemProto;
   });
 });
